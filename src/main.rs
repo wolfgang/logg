0a953cda7f46cc228bf53ef72121e58ff4cb0f1b
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-19 06:30:23 +0200

    Humble beginnings

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io::prelude::*;
+use std::io::BufWriter;
+use std::fs::OpenOptions;
+use std::env;
+use std::fmt;
+
+fn main() -> std::io::Result<()> {
+	let args: Vec<String> = env::args().collect();
+	println!("{:?}", args);
+	let file = OpenOptions::new().append(true).open("logg.txt")?;
+    let mut file = BufWriter::new(file);
+
+    let s = format!("{}\n", args[1]);
+
+    file.write_all(s.as_bytes())?;
+
+    Ok(())
+
+}
