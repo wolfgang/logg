17f3d334fe49491f4d635f6c49af627a5e923707
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-22 08:48:42 +0200

    Add learning test for serde_json lib

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+
 use std::io::prelude::*;
 use std::io::BufWriter;
 use std::fs::OpenOptions;
 use std::env;
+
+extern crate serde_json;
 
 fn main() -> std::io::Result<()> {
 	let args: Vec<String> = env::args().collect();
@@ -19,8 +22,28 @@ fn main() -> std::io::Result<()> {
 
 #[cfg(test)]
 mod tests {
+	use serde_json;
+	
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn serde_json_smoketest() {
+        let data_str = r#"{
+        	"some_str": "John",
+        	"some_int": 1234,
+        	"some_list": [1, 2]
+        }
+        "#;
+
+        let v: serde_json::Value = serde_json::from_str(data_str).unwrap();
+        assert_eq!("John", v["some_str"]);
+        assert_eq!(1234, v["some_int"]);
+        assert_eq!(1, v["some_list"][0]);
+        assert_eq!(2, v["some_list"][1]);
+
+        let l = &v["some_list"];
+        assert_eq!(1, l[0]);
+        assert_eq!(2, l[1]);
+
+
+
     }
 }
