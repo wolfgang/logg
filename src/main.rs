3718760f5756d7d620494b69d887247ccc505e96
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-22 11:26:55 +0200

    write a sample json string to the logg

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ use std::io::BufWriter;
 use std::fs::OpenOptions;
 use std::env;
 
+#[macro_use]
+extern crate serde_json;
+
 fn main() -> std::io::Result<()> {
 	let args: Vec<String> = env::args().collect();
 	println!("{:?}", args);
 	let file = OpenOptions::new().append(true).open("logg.txt")?;
     let mut file = BufWriter::new(file);
 
-    let s = format!("{}\n", args[1]);
+    let body = format!("{}\n", args[1]);
 
-    file.write_all(s.as_bytes())?;
+    let entry_json = json!({
+    	"cat": "some category",
+    	"body": body
+    });
+
+
+    file.write_all(entry_json.to_string().as_bytes())?;
 
     Ok(())
 
