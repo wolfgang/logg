6cd450289b5306c92e00c64404af9b86c10dd91b
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-22 10:54:27 +0200

    Add some more exploratory tests regarding error handling

--- tests/serde_json_test.rs
+++ tests/serde_json_test.rs
@@ -1,5 +1,16 @@
 extern crate serde_json;
 
+const INVALID_JSON: &'static str = "this is invalid json";
+
+const VALID_JSON: &'static str = 
+    r#"{
+        "some_str": "John",
+        "some_int": 1234,
+        "some_list": [1, 2]
+    }
+    "#;
+
+
 #[test]
 fn from_str_to_untyped() {
     let data_str = r#"{
@@ -23,20 +34,34 @@ fn from_str_to_untyped() {
 #[test]
 #[should_panic]
 fn panics_if_json_is_invalid() {
-    let data_str = "this is invalid json";
-
-    let _: serde_json::Value = serde_json::from_str(data_str).unwrap();
-
+    let _: serde_json::Value = serde_json::from_str(INVALID_JSON).unwrap();
 }
 
 #[test]
 fn can_examine_error() {
-    let data_str = "this is invalid json";
-
-    serde_json::from_str(data_str).unwrap_or_else(|err| {
+    serde_json::from_str(INVALID_JSON).unwrap_or_else(|err| {
         assert!(err.is_syntax());
         assert_eq!(1, err.line());
     });
+}
+
+#[test]
+fn can_propagate_error() {
+    let result = try_from_str(VALID_JSON);
+
+    let value = match result {
+        Ok(value) => value,
+        Err(error) => {
+            panic!("Could not parse string: {:?}", error)
+        },
+    };
+
+    assert_eq!("John", value["some_str"]);
+}
+
+fn try_from_str(data_str: &str) -> Result<serde_json::Value, serde_json::Error>{
+    let v: serde_json::Value = serde_json::from_str(data_str)?;
+    Ok(v)
 
 }
 
