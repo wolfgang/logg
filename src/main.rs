7c79446477884ff6bde331cfc727141cf9772683
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-22 17:33:44 +0200

    Push error handling further down to functions

--- src/main.rs
+++ src/main.rs
@@ -38,16 +38,15 @@ fn main() -> std::io::Result<()> {
 
 fn get_file_contents_as_json() -> serde_json::Value {
 	let mut contents = String::new();
- 	get_file_contents(&mut contents).expect("Failed to get contents from file");
+ 	get_file_contents(&mut contents);
     serde_json::from_str(&contents).expect("Failed to parse json from file")
 }
 
-fn get_file_contents(result: &mut String) -> Result<(), std::io::Error>  {
+fn get_file_contents(result: &mut String)  {
 	let file_for_read = OpenOptions::new()
 					.read(true)
                     .open("logg.txt").expect("Open for read failed");
     let mut file_for_read = BufReader::new(file_for_read);
 
- 	file_for_read.read_to_string(result)?;
- 	Ok(())
+ 	file_for_read.read_to_string(result).expect("Read from file failed");
 }
