8c8ede56c873544fbf7720e2dbd388f2b19af287
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-22 14:00:24 +0200

    Read file contents, replace a json key, write back file contents

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
 
 use std::io::prelude::*;
-use std::io::BufWriter;
-use std::fs::OpenOptions;
+use std::io::{BufWriter, BufReader};
+use std::fs::{OpenOptions, File};
+use std::fs;
 use std::env;
 
-#[macro_use]
 extern crate serde_json;
 
 fn main() -> std::io::Result<()> {
 	let args: Vec<String> = env::args().collect();
-	let file = OpenOptions::new().append(true).open("logg.txt")?;
+ 	let contents = fs::read_to_string("logg.txt")?;
+	let file = OpenOptions::new()
+					.write(true)
+					.truncate(true)
+                    .open("logg.txt").expect("Open failed");
     let mut file = BufWriter::new(file);
 
     let body = format!("{}\n", args[1]);
 
-    let entry_json = json!({
-    	"some_category": {
-    		"entries": [{
-    			"body": body 	
-    		}
-    		]
-    	}
-    });
+    let mut json: serde_json::Value = serde_json::from_str(&contents).unwrap();
+    json["some_category"]["entries"][0]["body"] = serde_json::Value::String(body);
 
+    println!("{:?}", json.to_string());
 
-    file.write_all(entry_json.to_string().as_bytes())?;
+    file.write_all(json.to_string().as_bytes()).expect("Write failed");
 
     Ok(())
 
