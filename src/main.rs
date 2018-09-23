08fd5c5fd77ef0c12a7f0ccebc51ab0b5e4d6ba1
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-23 13:41:12 +0200

    Match first arg as command

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,17 @@ use logg::cmd;
 fn main() -> std::io::Result<()> {
 	let args: Vec<String> = env::args().collect();
 
-	if args[1] == "a" {
-    	cmd::add(&args[2..]);
-	} else {
-		println!("Invalid command: {}", args[1]);
-		process::exit(1);
+	let cmd = &args[1];
+
+	match cmd as &str {
+		"a" => cmd::add(&args[2..]),
+		"l" => println!("LISTING"),
+		_ => {
+			println!("Invalid command: {}", args[1]);
+			process::exit(1);
+		}
+
 	}
 
     Ok(())
-
 }
