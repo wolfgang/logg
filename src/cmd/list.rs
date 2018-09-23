297eec149736bf01fb60f32d94b2b042ead0775c
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-23 14:34:39 +0200

    Protype list command

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -1,3 +1,19 @@
+use serde_json;
+
 pub(super) fn execute() {
 	println!("LISTING");
+	let json: serde_json::Value = ::core::io::get_file_contents_as_json();
+
+
+	let obj = json.as_object().unwrap();
+
+	for cat_name in obj.keys() {
+		let cat = &obj[cat_name];
+		println!("{:?}", cat_name);
+		let entries = &cat["entries"].as_array().unwrap();
+		for entry in entries.iter() {
+			println!("....{:?}", entry["body"].to_string());
+		}
+	}
+
 }
