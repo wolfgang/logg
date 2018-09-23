7dd35b33a36f1e01f299e0566acb5055e3d454c2
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-23 12:32:06 +0200

    Add unit tests for logg::add_entry_to_json

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,42 @@ pub fn add_entry_to_json(json: &mut serde_json::Value, cat: &str, body: &str) {
     	entries_ref.push(new_entry);
     }
 }
+
+#[cfg(test)]
+mod test {
+	use super::*;
+    #[test]
+    fn can_add_entry_to_empty_json() {
+    	let mut json = json!({});
+    	add_entry_to_json(&mut json, "some_category", "some_body");
+    	assert_eq!(
+    		json!({"some_category": {"entries": [{"body": "some_body"}]}}),
+    		json);
+    }
+    #[test]
+	fn can_add_second_category() {
+		let mut json =json!({"category_1": {"entries": [{"body": "body_1"}]}});
+		add_entry_to_json(&mut json, "category_2", "body_2");
+		assert_eq!(
+			json!({
+				"category_1": {"entries": [{"body": "body_1"}]},
+				"category_2": {"entries": [{"body": "body_2"}]}
+			}),
+			json);
+	}
+	#[test]
+	fn can_add_entry_to_existing_category() {
+		let mut json =json!({"category_1": {"entries": [{"body": "body_1"}]}});
+		add_entry_to_json(&mut json, "category_1", "body_2");
+		assert_eq!(
+			json!({
+				"category_1": {
+					"entries": [{"body": "body_1"}, {"body": "body_2"}]}
+			}),
+			json);
+
+	}    
+
+
+
+}
