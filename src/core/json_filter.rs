b22af0592c232d4e1e84898ad21cfcc2c49ed234
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-09-24 19:45:56 +0200

    Clean up

--- src/core/json_filter.rs
+++ src/core/json_filter.rs
@@ -1,23 +1,6 @@
 use serde_json;
 use std::collections::HashMap;
 
-pub fn by_category(cat: &str, json: &serde_json::Value) -> serde_json::Value {
-	let mut result = json!({});
-
-	let obj = json.as_object().unwrap();
-
-	for k in obj.keys() {
-		if cat == "" || cat == k {
-			let s = obj[k].to_string();
-			result[k] = serde_json::from_str(&s).unwrap();
-		}
-	}
-
-	let json_str =  result.to_string();
-	serde_json::from_str(&json_str).unwrap()
-}
-
-
 pub struct SearchResult<'a> {
     pub category: String,
     pub entries: Vec<&'a serde_json::Value>
@@ -45,6 +28,8 @@ pub fn by_body<'a>(_search_str: &str, json: &'a serde_json::Value) -> HashMap<St
 		let entries = &cat["entries"];
 		let mut matching_entries = Vec::new();
 
+        let sr = SearchResult::new(cat_name.to_string());
+
 		for entry in entries.as_array().unwrap() {
 			matching_entries.push(entry);
 		}
@@ -69,8 +54,8 @@ mod test {
 
     #[test]
     fn search_result_add_entries() {
-        let entry1 = serde_json::from_str(r#"{ "body": "some body_1"}"#).unwrap();
-        let entry2 = serde_json::from_str(r#"{ "body": "some body_2"}"#).unwrap();
+        let entry1 = entry_with_body("some_body_1");
+        let entry2 = entry_with_body("some_body_2");
         let mut sr = SearchResult::new(String::from("some_category"));
         sr.add(&entry1);
         sr.add(&entry2);
@@ -81,27 +66,6 @@ mod test {
 
 
     #[test]
-    fn return_full_input_if_cat_is_empty_string() {
-    	let json = json!({"cat1": {"entries": [{"body": "some body"}]}});
-    	let json_result = by_category("", &json);
-    	assert_eq!(json, json_result);
-    }
-
-    #[test]
-    fn return_full_only_give_category() {
-    	let json = json!({
-    		"cat1": {"entries": [{"body": "some body"}]},
-    		"cat2": {"entries": [{"body": "some body cat2"}]},
-    	});
-		let expected_result = json!({
-    		"cat2": {"entries": [{"body": "some body cat2"}]}
-    	});
-
-    	let json_result = by_category("cat2", &json);
-    	assert_eq!(expected_result, json_result);
-    }
-
-    #[test]
     fn return_full_input_if_search_is_empty_string() {
     	// let json = json!({"cat1": {"entries": [{"body": "some body"}]}});
     	// let result = by_body("", &json);
@@ -109,6 +73,10 @@ mod test {
     	// let s = String::from("cat1");
     	// let entries = result.get(&s);
 
+    }
+
+    fn entry_with_body(body: &str) -> serde_json::Value {
+        json!({"body": body.to_string()})
     }
 
 
