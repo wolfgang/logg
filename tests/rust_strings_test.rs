d952800d2bee813ec3b45a82ee548d87542f6397
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-10-01 20:31:40 +0200

    Add test that demonstrates the use of str.trim_matches

--- tests/rust_strings_test.rs
+++ tests/rust_strings_test.rs
@@ -5,10 +5,9 @@ fn split_by_newlines() {
     assert_eq!(vec!("line1", "line2", "line3"), parts);
 }
 
-// use std::fs;
-// #[test]
-// fn split_by_newlines_from_file() {
-// 	let contents = fs::read_to_string("/tmp/logg_tmp.txt").expect("Failed to read file contents");
-// 	let parts: Vec<&str> = contents.split('\n').collect();
-// 	assert_eq!(vec!("This is entry line 1",  "And this is line 2", "Line 3 is here", ""), parts );
-// }
+#[test] 
+fn remove_enclosing_quotes() {
+	let s = "\"in quotes\"";
+	let trimmed = s.trim_matches('"');
+	assert_eq!("in quotes", trimmed);
+}
