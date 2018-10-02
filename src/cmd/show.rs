cba186bb45f975ea6b0cd69bfcd526b55ef6659c
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-10-02 20:51:30 +0200

    Prototype full functionality of show command

--- src/cmd/show.rs
+++ src/cmd/show.rs
@@ -11,14 +11,21 @@ pub(super) fn execute(args: &[String]) {
 		else {
 			let cat = &args[2];
 			if cat == &result.category {
-				println!("> {}", cat);
-				let mut index = 0;
-				for entry in result.entries {
-					let body_as_str =  ::core::json::get_body_as_str(&entry);
-					let lines: Vec<&str> = body_as_str.lines().collect();
-					let more = if lines.len() > 1 { "[...]" } else { "" };
-					println!("[{}] {} {}", index, lines[0], more);
-					index = index + 1;
+				if args.len()==4 {
+					let index = args[3].parse::<usize>().unwrap();
+					let body_as_str =  ::core::json::get_body_as_str(result.entries[index]);
+					println!("> {}[{}]\n----------\n{}", cat, index, body_as_str);					
+				}
+				else {
+					println!("> {}", cat);
+					let mut index = 0;
+					for entry in result.entries {
+						let body_as_str =  ::core::json::get_body_as_str(&entry);
+						let lines: Vec<&str> = body_as_str.lines().collect();
+						let more = if lines.len() > 1 { "[...]" } else { "" };
+						println!("[{}] {} {}", index, lines[0], more);
+						index = index + 1;
+					}
 				}
 			}
 		}
