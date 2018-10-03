4743d70337ec4bee74ee932bda5cc354d98cd560
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-10-03 20:51:36 +0200

    Show all matches of search in a 'toc'

--- src/cmd/search.rs
+++ src/cmd/search.rs
@@ -1,15 +1,12 @@
 use serde_json;
-use ::core::{io, json, json_filter, display};
+use ::core::{io, json_filter, display};
 
 pub(super) fn execute(args: &[String]) {
 	let search_str = get_search_string(args);
 	let json: serde_json::Value = io::get_file_contents_as_json();
 	let results = json_filter::by_body(search_str, &json);
 	for result in results {
-		println!("> {}\n==========", result.category);
-		for entry in result.entries {
-			println!("{}\n-----", json::get_body_as_str(&entry));
-		}
+		display::show_toc_for_search_result(&result);
 	}
 }
 
