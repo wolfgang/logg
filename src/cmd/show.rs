9069243fd402bd597bc23fb668c918b8173c382b
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-10-03 20:45:46 +0200

    import some core libs directly

--- src/cmd/show.rs
+++ src/cmd/show.rs
@@ -1,17 +1,17 @@
 use serde_json;
-
+use ::core::{io, json, json_filter};
 
 pub(super) fn execute(args: &[String]) {
-	let json: serde_json::Value = ::core::io::get_file_contents_as_json();
+	let json: serde_json::Value = io::get_file_contents_as_json();
 	if args.len()==2 {
 		return show_all(&json)
 	}
 
 	let cat = &args[2];
-	let result = ::core::json_filter::by_category(cat, &json);
+	let result = json_filter::by_category(cat, &json);
 	if args.len()==4 {
 		let index = args[3].parse::<usize>().unwrap();
-		let body_as_str =  ::core::json::get_body_as_str(result.entries[index]);
+		let body_as_str =  json::get_body_as_str(result.entries[index]);
 		println!("> {}[{}]\n----------\n{}", cat, index, body_as_str);					
 	}
 	else {
@@ -20,17 +20,17 @@ pub(super) fn execute(args: &[String]) {
 }
 
 fn show_all(json: &serde_json::Value) {
-	let results = ::core::json_filter::by_body("", json);
+	let results = json_filter::by_body("", json);
 	for result in results {
 		println!("> {} ({})", result.category, result.entries.len());
 	}
 }
 
-fn show_toc_for_search_result(result: &::core::json_filter::SearchResult) {
+fn show_toc_for_search_result(result: &json_filter::SearchResult) {
 	println!("> {}", result.category);
 	let mut index = 0;
 	for entry in &result.entries {
-		let body_as_str =  ::core::json::get_body_as_str(&entry);
+		let body_as_str =  json::get_body_as_str(&entry);
 		let lines: Vec<&str> = body_as_str.lines().collect();
 		let more = if lines.len() > 1 { "[...]" } else { "" };
 		println!("[{}] {} {}", index, lines[0], more);
