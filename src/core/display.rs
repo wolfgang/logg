ef13b9047de2cc603ba87f9762c6c8be69db9b86
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-10-03 21:21:10 +0200

    Show id in toc

--- src/core/display.rs
+++ src/core/display.rs
@@ -10,12 +10,11 @@ pub fn show_toc(json: &serde_json::Value) {
 
 pub fn show_toc_for_search_result(result: &json_filter::SearchResult) {
 	println!("> {}", result.category);
-	let mut index = 0;
 	for entry in &result.entries {
 		let body_as_str =  json::get_body_as_str(&entry);
+		let id = entry["id"].as_u64().unwrap();
 		let lines: Vec<&str> = body_as_str.lines().collect();
 		let more = if lines.len() > 1 { "[...]" } else { "" };
-		println!("[{}] {} {}", index, lines[0], more);
-		index = index + 1;
+		println!("[{}] {} {}", id, lines[0], more);
 	}
 }
