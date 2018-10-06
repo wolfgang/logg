fea979c83df4fca3479ad7b6a4a776ec9d3c99c1
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-10-06 10:05:54 +0200

    Add alias BoxedResult<T>

--- src/core/error.rs
+++ src/core/error.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
-pub type EmptyBoxedResult=Result<(), Box<Error>>;
+pub type BoxedResult<T> = Result<T, Box<Error>>;
+pub type EmptyBoxedResult = BoxedResult<()>;
 
 pub fn simple_error(text: &str) -> Result<(), Box<Error>> {
 	Err(Box::from(text))
