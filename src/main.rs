16f604fe2a4e27d1e4a27e1ff3f3c2b2023d6ad6
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-10-06 09:16:44 +0200

    Add generic function run_cmd WIP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate logg;
 
 use std::env;
+use std::error::Error;
 use std::process;
-use logg::cmd;
+use logg::cmd::*;
 
 
 fn main() -> std::io::Result<()> {
@@ -11,7 +12,7 @@ fn main() -> std::io::Result<()> {
 	let cmd = &args[1];
 
 	let result = match cmd as &str {
-		"a" | "add" => cmd_add(&args[2..]),
+		"a" | "add" => run_cmd(&add::execute, &args[2..]),
 		// "f" | "find" => cmd::search::execute(&args),
 		// "s" | "show" => cmd::show::execute(&args),
 		_ => {
@@ -28,8 +29,12 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn cmd_add(args: &[String]) -> bool {
-	match cmd::add::execute(args) {
+type BoxedError=Result<(), Box<Error>>;
+
+type CmdFn=&'static Fn(&[String]) -> BoxedError;
+
+fn run_cmd(cmd_fn: CmdFn,  args: &[String]) -> bool {
+	match cmd_fn(args) {
 		Ok(()) => true,
 		Err(err) =>  {
 			println!("Error: {}", err);
