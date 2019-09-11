090f83cfd61a6e5951d2492198cf96ce5075c2d9
wolfgang <wolfgang.deutsch@gmail.com>
2019-09-11 14:33:10 +0200

    log file path can be set by env var LOGG_FILE

--- src/core/io.rs
+++ src/core/io.rs
@@ -12,7 +12,7 @@ pub fn init_log() {
         DirBuilder::new()
             .recursive(false)
             .create(get_home_dir()).expect("Failed to create home dir");
-    
+
         let mut file = File::create(get_log_file()).expect("Create file failed");
         file.write_all(b"{}").expect("Init file failed");
     }
@@ -28,36 +28,36 @@ pub fn read_log() -> serde_json::Value {
 
 pub fn write_log(json: &serde_json::Value) {
     let file = OpenOptions::new()
-                    .write(true)
-                    .truncate(true)
-                    .open(get_log_file()).expect("Open for write failed");
+        .write(true)
+        .truncate(true)
+        .open(get_log_file()).expect("Open for write failed");
     let mut file = BufWriter::new(file);
 
     file.write_all(serde_json::to_string_pretty(json).unwrap().as_bytes()).expect("Write failed");
 }
 
 
-fn read_log_as_string(result: &mut String)  {
+fn read_log_as_string(result: &mut String) {
     let log_file = OpenOptions::new()
-                    .read(true)
-                    .open(get_log_file()).expect("Open for read failed");
+        .read(true)
+        .open(get_log_file()).expect("Open for read failed");
     let mut log_file = BufReader::new(log_file);
 
     log_file.read_to_string(result).expect("Read from file failed");
 }
 
 fn get_log_file() -> String {
-    format!("{}/{}", get_home_dir(), core::LOG_FILE)
-}
-
-fn get_home_dir() -> String {
     match env::var("LOGG_FILE") {
         Ok(val) => val,
         Err(_) => {
-            let home = env::var("HOME").unwrap();
-            format!("{}/{}", home, core::LOGG_HOME)
+            format!("{}/{}", get_home_dir(), core::LOG_FILE)
         }
     }
+}
+
+fn get_home_dir() -> String {
+    let home = env::var("HOME").unwrap();
+    format!("{}/{}", home, core::LOGG_HOME)
 }
 
 
